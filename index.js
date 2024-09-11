4bf1d47a1562be6d7ec603d2324cbb4d51c05772
Anees <aneesnafeesimmu@gmail.com>
2024-09-11 23:56:36 +0530

    log-removed

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const authMiddleware = (req, res, next) => {
     const token = authHeader.split(' ')[1]
     try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        console.log(decoded)
+        // console.log(decoded)
         req.user = decoded
         next()
     }
